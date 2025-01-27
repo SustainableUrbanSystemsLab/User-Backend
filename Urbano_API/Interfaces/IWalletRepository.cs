468037172ca4f35e8f1fb89946487b57789df031
rohitd42 <rohitdudala@gmail.com>
2025-01-27 15:48:06 -0500

    Fix Add-Remove-Verify Token, GetBalance

--- Urbano_API/Interfaces/IWalletRepository.cs
+++ Urbano_API/Interfaces/IWalletRepository.cs
@@ -1,20 +1,23 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Urbano_API.Models;
 
-namespace Urbano_API.Interfaces;
-
-public interface IWalletRepository
+namespace Urbano_API.Interfaces
 {
-    public Task<List<Wallet>> GetAsync();
+    public interface IWalletRepository
+    {
+        Task<List<Wallet>> GetAsync();
+        Task<Wallet?> GetAsync(string id);
+        Task<Wallet?> GetWalletByUserIdAsync(string userId);
+        Task CreateAsync(Wallet wallet);
+        Task UpdateAsync(string id, Wallet wallet);
+        Task RemoveAsync(string id);
 
-    public Task<Wallet?> GetAsync(string id);
-
-    public Task<Wallet?> GetWalletByUserIdAsync(string userId);
-
-    public Task CreateAsync(Wallet wallet);
-
-    public Task UpdateAsync(string id, Wallet wallet);
-
-    public Task RemoveAsync(string id);
-
+        // Add missing methods
+        Task AddTokenAsync(string userId, string tokenType, int quantity);
+        Task<bool> RemoveTokenAsync(string userId, string tokenType, int quantity);
+        Task<Dictionary<string, int>> GetBalanceAsync(string userId);
+        Task<bool> VerifyTokenAsync(string userId, string tokenType, int requiredQuantity);
+    }
 }
 
