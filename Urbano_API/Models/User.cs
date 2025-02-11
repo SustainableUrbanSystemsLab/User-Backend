d55295e4f168dbf38ae3258c1b3e1e53db269e24
avegesna8 <abhinn.varma@gmail.com>
2025-02-10 20:31:22 -0500

    Removed Saving Login Date for Registering

--- Urbano_API/Models/User.cs
+++ Urbano_API/Models/User.cs
@@ -33,7 +33,7 @@ public class User
     [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
     public DateTime Date { get; set; } = DateTime.Today;
 
-    public DateTime LastLoginDate {get; set; } = DateTime.Today;
+    public DateTime LastLoginDate {get; set; } = DateTime.MinValue;
 
     public User()
     {
