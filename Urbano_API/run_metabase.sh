c9dd516e0a9a60c3e83df4810185d7d865d03b0a
rohitd42 <rohitdudala@gmail.com>
2025-04-20 16:00:09 -0400

    Added metabase script

---
+++ Urbano_API/run_metabase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Stop and remove existing container if it's already running
+docker stop metabase 2>/dev/null
+docker rm metabase 2>/dev/null
+
+# Run Metabase container
+docker run -d -p 3000:3000 \
+  --name metabase \
+  --dns 8.8.8.8 \
+  metabase/metabase:latest
+
